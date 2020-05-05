137e24919d72ef280f030558c38c5698f251eb30
Ahmad Karlam <ahmad.karlam@gmail.com>
2020-05-05 13:52:44 +0700

    feat: add seeder

---
+++ databases/seeder/warehouses.sql
@@ -0,0 +1,8 @@
+INSERT INTO go_shipping.warehouses (id, code, stock, x, y, created_at, updated_at) VALUES (1, 'A', 10, 5, 18, '2020-05-05 13:14:03', null);
+INSERT INTO go_shipping.warehouses (id, code, stock, x, y, created_at, updated_at) VALUES (2, 'B', 10, 19, 17, '2020-05-05 13:14:03', null);
+INSERT INTO go_shipping.warehouses (id, code, stock, x, y, created_at, updated_at) VALUES (3, 'C', 10, 10, 18, '2020-05-05 13:14:03', null);
+INSERT INTO go_shipping.warehouses (id, code, stock, x, y, created_at, updated_at) VALUES (4, 'D', 10, 21, 14, '2020-05-05 13:14:03', null);
+INSERT INTO go_shipping.warehouses (id, code, stock, x, y, created_at, updated_at) VALUES (5, 'E', 10, 8, 10, '2020-05-05 13:14:03', null);
+INSERT INTO go_shipping.warehouses (id, code, stock, x, y, created_at, updated_at) VALUES (6, 'F', 10, 16, 9, '2020-05-05 13:14:03', null);
+INSERT INTO go_shipping.warehouses (id, code, stock, x, y, created_at, updated_at) VALUES (7, 'G', 10, 11, 5, '2020-05-05 13:14:03', null);
+INSERT INTO go_shipping.warehouses (id, code, stock, x, y, created_at, updated_at) VALUES (8, 'H', 10, 17, 4, '2020-05-05 13:14:03', null);
